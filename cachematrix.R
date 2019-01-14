ad02d4bf6667711ad213e9555178ae09693582ce
Citrusloop <rvermajhansi@gmail.com>
2019-01-14 18:57:08 +0530

    clean code for matrix inversion and extraction

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R find the inverse of a matrix and if inverse is in cache then extract from there
+## considering matrix passed is invertible
 
-## Write a short comment describing this function
+## makeCacheMatrix() storing the inverse in cache and having function for getting,setting the matrix
+## and also getting and setting the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+
+    inv=NULL
+
+    set<-function(mat,nr,nc){
+        x<<-matrix(mat,nr,nc)
+        inv<<-NULL
+  
+    }
+
+    setinv<-function(p){
+        inv<<-p
+    }
+      getinv<-function(){inv}
+    get<-function(){x}
+    
+  
+    list(set = set, get = get,
+    setinv = setinv,
+    getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() function finds the inverse of the given matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        
+        if(!is.null(x$getinv())){
+            # print("from cache")
+            return (x$getinv())
+        }
+        
+        inverse=solve(x$get())
+        x$setinv(inverse)
+        inverse
 }
